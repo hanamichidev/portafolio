b6ff25f1260691bed9322067c83de9832cdd77b5
Emmanuel Cruz <emmanuelcruz.personal@gmail.com>
2025-12-31 13:01:16 -0600

    fix: erro pagina en blanco

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portafolio/', // Cambia 'portfolio' por el nombre exacto de tu repositorio
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -18,6 +18,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
