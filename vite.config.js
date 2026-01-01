01d8f1991fd1fb278829696683e0edd7ec611b2d
Emmanuel Cruz <emmanuelcruz.personal@gmail.com>
2026-01-01 00:41:13 -0600

    arreeglo github actions

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/portafolio/' : '/',
+  base: '/portafolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
